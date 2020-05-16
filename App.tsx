87649dff34cea4d3ce3c7a17edef6275eaee36b4
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-05-16 12:01:07 +0300

    Add eslint prettier to the project

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.content}>Dicision App</Text>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,5 +12,13 @@ const styles = StyleSheet.create({
     fontSize: 60,
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>Dicision App</Text>
+    </View>
+  );
+};
 
 export default App;
