f54f5bc86d109f568351c80b31995e11ee4641af
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-05-30 23:47:08 +0300

    Add types for changeHandlers

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -5,6 +5,8 @@ type Config<T> = {
   onSubmit: (values: T) => void;
 };
 
+type ChangeHandler<T> = { [K in keyof T]: (text: string) => void };
+
 export const useForm = <T>({ initialValues, onSubmit }: Config<T>) => {
   const [values, setValues] = useState<T>(initialValues);
 
@@ -18,14 +20,13 @@ export const useForm = <T>({ initialValues, onSubmit }: Config<T>) => {
     setValues((prevState) => ({ ...prevState, [fieldName]: text }));
   };
 
-  const createChangeHandlers = (
-    val: T
-  ): { [key: string]: (text: string) => void } => {
+  const createChangeHandlers = (val: T): ChangeHandler<T> => {
     return Object.keys(val).reduce(
       (acum, key) => ({ ...acum, [key]: handleChange(key as keyof T) }),
-      {}
+      {} as ChangeHandler<T>
     );
   };
+
   const changeHandlers = createChangeHandlers(values);
 
   const disabled = Object.values(values).some((value) => !value);
