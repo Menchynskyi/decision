70ce919d90741e2d2c42956cc59d2d5d091a0f66
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-05-31 13:09:07 +0300

    Wrap forms in TouchableWithoutFeedback

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from './SignIn.style';
 import { useForm } from '../../hooks';
@@ -21,32 +28,34 @@ export const SignIn: React.FC = () => {
   });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.form}>
-        <TextInput
-          placeholder="Username or Email"
-          style={styles.input}
-          value={values.username}
-          onChangeText={changeHandlers.username}
-        />
-        <TextInput
-          placeholder="Password"
-          style={styles.input}
-          value={values.password}
-          onChangeText={changeHandlers.password}
-          secureTextEntry
-        />
-        <TouchableOpacity disabled={dirty} onPress={handleSubmit}>
-          <View style={[styles.submitButton, dirty && styles.disabled]}>
-            <Text style={styles.submitText}>Sign In</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            placeholder="Username or Email"
+            style={styles.input}
+            value={values.username}
+            onChangeText={changeHandlers.username}
+          />
+          <TextInput
+            placeholder="Password"
+            style={styles.input}
+            value={values.password}
+            onChangeText={changeHandlers.password}
+            secureTextEntry
+          />
+          <TouchableOpacity disabled={dirty} onPress={handleSubmit}>
+            <View style={[styles.submitButton, dirty && styles.disabled]}>
+              <Text style={styles.submitText}>Sign In</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity onPress={handlePress}>
+          <View>
+            <Text style={styles.text}>or Sign Up</Text>
           </View>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={handlePress}>
-        <View>
-          <Text style={styles.text}>or Sign Up</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
